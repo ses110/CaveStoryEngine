144e632b7d7236458c9a6f572b21812f5bf4ce83
ses110 <ses110@gmail.com>
2016-02-09 10:16:09 -0600

    Animated character sprite. End of EP 4

--- src/game.cc
+++ src/game.cc
@@ -3,11 +3,13 @@
 #include <SDL/SDL.h>
 
 #include "graphics.h"
-#include "sprite.h"
+#include "animated_sprite.h"
 
 namespace {
     const int kFPS = 60;
 }
+
+int Game::kTileSize = 32;
 
 Game::Game() {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -22,9 +24,10 @@ void Game::eventLoop() {
     Graphics graphics;
     SDL_Event event;
 
-    sprite_.reset(new Sprite("content/MyChar.bmp", 0,0, 32, 32));
+    sprite_.reset(new AnimatedSprite("content/MyChar.bmp", 0,0, kTileSize, kTileSize, 15, 3));
 
     bool running = true;
+    int last_update_time = SDL_GetTicks();
     while (running) {
         const int start_time = SDL_GetTicks();
         while (SDL_PollEvent(&event)) {
@@ -38,7 +41,10 @@ void Game::eventLoop() {
                     break;
             }
         }
-        update();
+        const int current_time_ms = SDL_GetTicks();
+        update(current_time_ms - last_update_time);
+        last_update_time = current_time_ms;
+
         draw(graphics);
         const int elapsed_time = SDL_GetTicks() - start_time;
         // SDL_Delay takes a Uint32. If elapsed_time longer than 1000/60 ms then you pass an underflowing number and app will hang
@@ -48,7 +54,8 @@ void Game::eventLoop() {
     }
 }
 
-void Game::update() {
+void Game::update(int elapsed_time_ms) {
+    sprite_->update(elapsed_time_ms);
 }
 void Game::draw(Graphics& graphics) {
     sprite_->draw(graphics, 320, 240);
